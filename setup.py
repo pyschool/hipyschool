c8e85425016e6097bf96da2284b7d8580a1e95bc
Pablo Ricco <pricco@gmail.com>
2015-07-07 01:41:18 -0300

    Add story as requirement.

--- setup.py
+++ setup.py
@@ -5,12 +5,14 @@ version = __import__('hipython').__version__
 setup(
     name='hipython',
     version=version,
-    packages=find_packages(),
     license='MIT',
     author='PySchool',
     url='https://github.com/pyschool/hipython/',
     description='The hipython workshop.',
-    include_package_data=True,
+    packages=find_packages(),
+    install_requires=[
+        'story',
+    ],
     entry_points={
         'console_scripts': [
             'hipython = hipython.Story:begin',
